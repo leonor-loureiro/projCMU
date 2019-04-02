ef7c434c728b9bc55b95fc992bf2db10e9f43fd0
leonor-loureiro <leonor.filipa@tecnico.ulisboa.pt>
2019-04-02 12:08:33 +0100

    Add user and album attributes

--- server/src/main/java/pt/ulisboa/ist/cmu/p2photo/server/data/Album.java
+++ server/src/main/java/pt/ulisboa/ist/cmu/p2photo/server/data/Album.java
@@ -1,4 +1,34 @@
 package pt.ulisboa.ist.cmu.p2photo.server.data;
 
+/**
+ * This class represents a P2Photo album
+ */
 public class Album {
+
+    //Album name
+    private String name;
+    //Album catalog url
+    private String url;
+
+
+    public Album(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
--- server/src/main/java/pt/ulisboa/ist/cmu/p2photo/server/data/User.java
+++ server/src/main/java/pt/ulisboa/ist/cmu/p2photo/server/data/User.java
@@ -1,4 +1,45 @@
 package pt.ulisboa.ist.cmu.p2photo.server.data;
 
+import java.util.List;
+
+/**
+ * This class represent a P2Photo user
+ */
 public class User {
+
+    // Username
+    private String username;
+    // Hashed password
+    private String password;
+    // User's albums
+    private List<Album> albums;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Album> getAlbums() {
+        return albums;
+    }
+
+    public void addAlbum(Album album){
+        albums.add(album);
+    }
 }
